665f6a0e04744f7d7c54a5678399f01ddb4a2262
tony nguyen <nhattruong1811@gmail.com>
2025-11-15 10:00:20 +0700

    flow authenticate

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { PATH } from '../configs';
 import { useDispatch } from 'react-redux';
 import { setUser } from '../redux/appSlice';
+import { UnauthoriedPage } from '../pages/other-pages';
+import Loader from '../components/loader';
 
 function AuthRoutes({ children }: React.PropsWithChildren) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+
   const access_token = window.localStorage.getItem('access_token');
 
   React.useEffect(() => {
-    if (!access_token) return;
+    if (!access_token) {
+      setIsLoading(false);
+      return;
+    };
 
     async function checkAuthenticated() {
       try {
+        await new Promise(resolve => setTimeout(resolve, 500));
         const user = {
             "id": "67b605dfb3917ce0353db8f0",
             "avatar": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/009d272e2b496aa0758a86a17eac5f7716a99133_full.jpg",
@@ -27,17 +35,18 @@ function AuthRoutes({ children }: React.PropsWithChildren) {
         console.log('checkAuthenticated error: ', error);
         window.localStorage.clear();
         navigate(PATH.UNAUTHORIED_PAGE)
+      } finally {
+        setIsLoading(false);
       }
     }
     checkAuthenticated();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [access_token])
 
-  if(!access_token) {
-    return <Navigate to={PATH.LOGIN} />
+  if(isLoading) {
+    return <Loader />
   }
 
-  if(!isAuthenticated) return null;
+  if(!isAuthenticated) return <UnauthoriedPage />;
 
   return (
     <>{children}</>
